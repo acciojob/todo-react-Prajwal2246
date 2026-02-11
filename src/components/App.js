8319c13589a0c8e96358a0b237f47743b8dbe50c
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-02-11 17:22:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
+import Todo from "./Todo";
 
 const App = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const handleAdd = () => {
+    if (!input.trim()) return;
+    setTodos([...todos, input]);
+    setInput("");
+  };
+
+  const deleteTodo = (id) => {
+    const filtered = todos.filter((_, i) => i != id);
+    setTodos(filtered);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <h3>To-Do List </h3>
 
-export default App
+      <input
+        type="text"
+        placeholder=""
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={handleAdd}>Add Todo</button>
+
+      <Todo todos={todos} deletetodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
